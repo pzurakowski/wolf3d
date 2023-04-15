92c4f2196fc84a04041b750060d1c4e8f3eb968f
tetragonites <elmehlan@gmail.com>
2023-04-15 16:42:19 +0200

    script for wolf agent motion actuation ~50% finished

--- Assets/Behaviors/WolfAgentBehavior.cs
+++ Assets/Behaviors/WolfAgentBehavior.cs
@@ -11,45 +11,60 @@ using Unity.MLAgents.Actuators;
 
 public class WolfAgent : Agent
 {
-
         // Actionable objects
-        ConfigurableJoint hip_l;
-        ConfigurableJoint hip_r;
-        ConfigurableJoint knee_l;
-        ConfigurableJoint knee_r;
-        ConfigurableJoint bheel_l;
-        ConfigurableJoint bheel_r;
-        ConfigurableJoint spine1;
-        ConfigurableJoint spine2;
-        ConfigurableJoint clavicle_l;
-        ConfigurableJoint clavicle_r;
-        ConfigurableJoint shoulder_l;
-        ConfigurableJoint shoulder_r;
-        ConfigurableJoint elbow_l;
-        ConfigurableJoint elbow_r;
-        ConfigurableJoint fheel_l;
-        ConfigurableJoint fheel_r;
-        ConfigurableJoint neck;
-        
-    // Start is called before the first frame update
-        void Start()
+        int countJoints = 17;
+        Rigidbody[] motors[countJoints];
+
+        // Other objects
+        int controlSignal = Vector3.zero;
+
+        // Start is called before the first frame update
+        public override void Start()
         {
-                // get objects that are part of the learning process
-                hip_l = GetComponent<
+                Dictionary<string, int> motorIndex = new Dictionary<string, int> {
+                        {"spine1", 0}, {"spine2", 1}, {"neck", 2},
+                        {"clavicle_l", 3}, {"shoulder_l", 4}, {"elbow_l", 5}, {"fheel_l", 6},
+                        {"clavicle_r", 7}, {"shoulder_r", 8}, {"elbow_r", 9}, {"fheel_r", 10},
+                        {"hip_l", 11}, {"knee_l", 12}, {"bheel_l", 13},
+                        {"hip_r", 14}, {"knee_r", 15}, {"bheel_r", 16}
+                }
+
+                Rigidbody[] allRigidbodies = GetComponentsInChildren<Rigidbody>();
+
+                foreach (Rigidbody rb in allRigidbodies) {
+                        int i = motorIndex[rb.gameObject.name];
+                        motors[i] = rb;
+                }
         }
 
-        void onEpisodeBegin() 
+        public override void onEpisodeBegin() 
         {
+                // TODO need to code in constraints:
+                //
+                // if wolf falls off the map, reset his position and velocity, get Target into a random position 
+        }
+        
 
 
-    void CollectObservations(VectorSensor sensor) 
-    {
+        void CollectObservations(VectorSensor sensor) 
+        {
+                // TODO need to collect obs and push them on to Sensor
+                //
+        }
 
-    }
-
-    void OnActionReceived(float[] vectorAction) 
-    {
-
-    }
-
+        // apply output of neural net as torque to each joint
+        void OnActionReceived(float[] vectorAction) 
+        {
+                for (int i = 0; i < countJoints; ++i) {
+                        // get torque
+                        controlSignal.x = vectorAction[3*i];
+                        controlSignal.y = vectorAction[3*i + 1];
+                        controlSignal.z = vectorAction[3*i + 2];
+                        // apply it to the motor
+                        motors[i].AddTorque(controlSignal);             // I wonder if the fact that
+                                                                          // we're applying torques 
+                                                                          // asynchronously will actually be visible
+                                                                          // in the simulation?
+                } 
+        }
 }
